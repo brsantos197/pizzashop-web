8b3a09f8e45396100fd18a31a5639c98f34eae7b
Bruno Santos <brsantos197@gmail.com>
2024-02-25 19:25:35 -0300

    feat: Notificações toast(sonner)

--- src/pages/app/sign-in.tsx
+++ src/pages/app/sign-in.tsx
@@ -1,6 +1,7 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
@@ -23,9 +24,24 @@ export const SignIn = () => {
   })
 
   const handleSignIn = async (data: SignInFormData) => {
-    console.log(data)
+    try {
+      console.log(data)
 
-    await new Promise((resolve) => setTimeout(resolve, 2000))
+      // throw new Error('Credenciais inválidas.')
+
+      await new Promise((resolve) => setTimeout(resolve, 2000))
+
+      toast.success('Enviamos um link de autenticação para seu e-mail.', {
+        action: {
+          label: 'Reenviar',
+          onClick: () => handleSignIn(data),
+        },
+      })
+    } catch (error) {
+      if (error instanceof Error) {
+        toast.error(error.message)
+      }
+    }
   }
 
   return (
