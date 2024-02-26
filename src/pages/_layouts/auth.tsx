7eab33b9a8f65545eb82dcd300cf10688251b0ff
Bruno Santos <brsantos197@gmail.com>
2024-02-26 00:08:04 -0300

    feat: Layout do app com cabeçalho

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -2,7 +2,7 @@ import { Pizza } from 'lucide-react'
 import { Outlet } from 'react-router-dom'
 export const AuthLayout = () => {
   return (
-    <div className="grid h-screen grid-cols-1 grid-rows-[1fr_250px] md:grid-cols-2 md:grid-rows-1">
+    <div className="grid h-screen grid-cols-1 grid-rows-[1fr_250px] antialiased md:grid-cols-2 md:grid-rows-1">
       <div className="order-2 flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground md:order-1">
         <div className="flex items-center gap-3 text-lg font-medium text-foreground">
           <Pizza className="h-5 w-5" />
