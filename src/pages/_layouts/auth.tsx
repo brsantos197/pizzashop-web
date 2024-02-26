e6f3d898920c2ca04d2fa1a6e5f10d7a70495ba3
Bruno Santos <brsantos197@gmail.com>
2024-02-25 23:50:26 -0300

    feat: Página cadastro de restaurante

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -2,8 +2,8 @@ import { Pizza } from 'lucide-react'
 import { Outlet } from 'react-router-dom'
 export const AuthLayout = () => {
   return (
-    <div className="grid h-screen grid-cols-2 ">
-      <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
+    <div className="grid h-screen grid-cols-1 grid-rows-[1fr_250px] md:grid-cols-2 md:grid-rows-1">
+      <div className="order-2 flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground md:order-1">
         <div className="flex items-center gap-3 text-lg font-medium text-foreground">
           <Pizza className="h-5 w-5" />
           <span className="semi-bold">pizza.shop</span>
@@ -12,7 +12,7 @@ export const AuthLayout = () => {
           Painel do parceiro &copy; pizza.shop - {new Date().getFullYear()}
         </footer>
       </div>
-      <div className="flex items-center justify-center">
+      <div className="relative order-1 flex items-center justify-center md:order-2">
         <Outlet />
       </div>
     </div>
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,6 +1,7 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
@@ -48,6 +49,9 @@ export const SignIn = () => {
     <>
       <Helmet title="Login" />
       <div className="p-8">
+        <Button asChild className="absolute right-8 top-8" variant="ghost">
+          <Link to="/sign-up">Novo estabelecimento</Link>
+        </Button>
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
@@ -65,6 +69,11 @@ export const SignIn = () => {
               <Button disabled={isSubmitting} className="w-full">
                 Acessar Painel
               </Button>
+              <Link to="sign-up">
+                <Button asChild className="absolute right-8 top-8">
+                  Novo estabelecimento
+                </Button>
+              </Link>
             </div>
           </form>
         </div>
